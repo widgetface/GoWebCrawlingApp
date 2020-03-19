ec406fcfd3dc48db14568661b18df4feb40b75dd
davet <daveenq@gmail.com>
2020-03-19 15:43:32 +0000

    Added a dependency

--- create-module/hello/hello.go
+++ create-module/hello/hello.go
@@ -1,5 +1,9 @@
 package hello
 
+import "rsc.io/quote"
+
+//import "rsc.io/quote/v2" for  specific version
+
 func Hello() string {
-	return "Hello, world."
+	return quote.Hello()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/widgetface/GoWebCrawlingApp
 
 go 1.14
+
+require rsc.io/quote v1.5.2
